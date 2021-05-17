051a5bc0f716fb46cd3a74050484e3ba343f2ed9
israfil <israfil3019@gmail.com>
2021-05-17 15:25:26 +0300

    fetch data add logos to buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,60 @@
 import "./Card.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import design from "../assets/design.svg";
+import growingMan from "../assets/growing-up-man.svg";
+import man from "../assets/man.svg";
+import woman from "../assets/woman.svg";
+import growingWoman from "../assets/growing-up-woman.svg";
+import email from "../assets/mail.svg";
+import location from "../assets/map.svg";
+import password from "../assets/padlock.svg";
+import phone from "../assets/phone.svg";
+import loadingGif from "../assets/loading.gif";
 
 const Card = () => {
+  const [randomUser, setRandomUser] = useState();
+
+  const fetchData = () => {
+    axios.get("https://randomuser.me/api/").then((res) => {
+      //   console.log(res);
+      setRandomUser(res.data.results[0]);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <div className="container">
         <div className="header">Dummy users profile info</div>
         <div className="card-container">
           <div className="card-header">
-            <div className="profil-photo"></div>
+            {/* <div><img src={loadingGif} alt="loading" /></div> */}
+            <div className="profil-photo">
+              <img src="" alt="profil" />
+            </div>
             <div className="profil-info">my name is</div>
           </div>
           <div className="logos">
             <button>
-              <img src="" alt="" />
+              <img src={woman} alt="man-woman" />
             </button>
             <button>
-              <img src="" alt="" />
+              <img src={email} alt="email" />
             </button>
             <button>
-              <img src="" alt="" />
+              <img src={growingMan} alt="age" />
             </button>
             <button>
-              <img src="" alt="" />
+              <img src={location} alt="map" />
             </button>
             <button>
-              <img src="" alt="" />
+              <img src={phone} alt="telephone" />
             </button>
             <button>
-              <img src="" alt="" />
+              <img src={password} alt="padlock" />
             </button>
           </div>
           <div className="buttons">
@@ -37,7 +64,7 @@ const Card = () => {
           <div className="addUser-container"></div>
         </div>
         <div className="footer">
-          <img src="" alt="" />
+          <img src={design} alt="design" />
         </div>
       </div>
     </div>
